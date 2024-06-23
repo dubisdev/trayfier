7181a679c7f7bea55c4dc30bc1e67ed08b0815a4
dubisdev <dubisdev@gmail.com>
2024-06-23 18:07:00 +0200

    chore: add persiostence to apps

--- src/stores/useTrayAppsStore.ts
+++ src/stores/useTrayAppsStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { TrayApp } from "../modules/TrayApp/domain/TrayApp";
 import { showTrayApp } from "../modules/TrayApp/application/showTrayApp";
 import { hideTrayApp } from "../modules/TrayApp/application/hideTrayApp";
@@ -13,28 +14,33 @@ type TrayAppStore = {
     deleteTrayApp: (trayAppId: TrayApp) => void;
 }
 
-export const useTrayAppsStore = create<TrayAppStore>()((set) => ({
-    trayApps: [],
+export const useTrayAppsStore = create<TrayAppStore>()(
+    persist(
+        (set) => ({
+            trayApps: [],
 
-    addTrayApp: (trayInfo, trayImage) => {
-        const trayApp = TrayApp.create({
-            ...trayInfo,
-            id: crypto.randomUUID(),
-            icon: trayImage
-        })
+            addTrayApp: (trayInfo, trayImage) => {
+                const trayApp = TrayApp.create({
+                    ...trayInfo,
+                    id: crypto.randomUUID(),
+                    icon: trayImage
+                })
 
-        set((state) => ({ trayApps: [...state.trayApps, trayApp] }))
+                set((state) => ({ trayApps: [...state.trayApps, trayApp] }))
 
-        saveAppImageUseCase(trayApp.appImage, tauriAppImageRepository)
+                saveAppImageUseCase(trayApp.appImage, tauriAppImageRepository)
 
-        showTrayApp(trayApp, tauriTrayAppVisibilityManager)
-    },
+                showTrayApp(trayApp, tauriTrayAppVisibilityManager)
+            },
 
-    deleteTrayApp: async (trayApp: TrayApp) => {
-        set((state) => ({ trayApps: state.trayApps.filter(app => app !== trayApp) }))
+            deleteTrayApp: async (trayApp: TrayApp) => {
+                set((state) => ({ trayApps: state.trayApps.filter(app => app !== trayApp) }))
 
-        await hideTrayApp(trayApp, tauriTrayAppVisibilityManager)
+                await hideTrayApp(trayApp, tauriTrayAppVisibilityManager)
 
-        await removeAppImageUseCase(trayApp.appImage.name, tauriAppImageRepository)
-    }
-}));
+                await removeAppImageUseCase(trayApp.appImage.name, tauriAppImageRepository)
+            }
+        }
+        ), { name: "tray-apps" }
+    )
+);
