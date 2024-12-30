26662ff3e0e7a49b213fbad9cc0b704b5c9840b3
dubisdev <dubisdev@gmail.com>
2024-12-30 21:14:26 +0100

    chore: Improve create form

--- src/components/CreateTrayAppForm.tsx
+++ src/components/CreateTrayAppForm.tsx
@@ -1,48 +1,101 @@
-import { FormEvent, useState } from "react";
 import { useTrayAppsStore } from "../stores/useTrayAppsStore";
 import { open } from "@tauri-apps/plugin-dialog";
 import { convertFileSrc } from "@tauri-apps/api/core";
 import { Icon } from "../modules/TrayApp/components/Icon";
+import * as z from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const CreateTrayAppFormSchema = z.object({
+    trayAppName: z.string().nonempty(),
+    trayAppIcon: z.string().nonempty(),
+    trayAppSrc: z.string().nonempty(),
+})
+
+type ICreateTrayAppForm = z.infer<typeof CreateTrayAppFormSchema>
 
 interface CreateTrayAppFormProps {
     onCreated: () => void
 }
 
 export const CreateTrayAppForm = (props: CreateTrayAppFormProps) => {
+    const form = useForm<ICreateTrayAppForm>({
+        resolver: zodResolver(CreateTrayAppFormSchema)
+    })
     const addTrayApp = useTrayAppsStore(s => s.addTrayApp)
 
-    const [iconSrc, setIconSrc] = useState<string | null>(null)
 
     const handleSelectImage = async () => {
         const filePath = await open({ multiple: false, filters: [{ name: "*", extensions: ["png"] }] })
 
-        setIconSrc(filePath)
+        if (!filePath) return form.setError("trayAppIcon", { message: "Please select an icon" })
+
+        form.clearErrors("trayAppIcon")
+        form.setValue("trayAppIcon", filePath)
     }
 
-    const handleCreateTrayApp = async (e: FormEvent, iconSrc: string | null) => {
-        e.preventDefault();
+    const handleCreateTrayApp: SubmitHandler<ICreateTrayAppForm> = async (data) => {
+        const { trayAppName, trayAppIcon, trayAppSrc } = data
 
-        const fields = new FormData(e.target as HTMLFormElement);
-
-        const name = fields.get("trayAppName");
-
-        if (!name) return
-
-        if (typeof name !== "string") return
-
-        if (!iconSrc) return
-
-        addTrayApp({ name, iconSrc, path: iconSrc })
+        addTrayApp({
+            name: trayAppName,
+            iconSrc: trayAppIcon,
+            path: trayAppSrc
+        })
 
         props.onCreated()
     }
 
-    return <form onSubmit={(e) => handleCreateTrayApp(e, iconSrc)} autoComplete="off">
-        <input name="trayAppName" />
-        <button onClick={handleSelectImage}>Select App Icon</button>
-        {iconSrc && <Icon src={convertFileSrc(iconSrc)} altName="Selected icon" />}
+    return <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleCreateTrayApp)} className="space-y-8">
+            <FormField
+                control={form.control}
+                name="trayAppName"
+                render={({ field }) => {
+                    return <FormItem>
+                        <FormLabel>App Name</FormLabel>
+                        <FormControl>
+                            <Input {...field} autoComplete="off" />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
 
-        <button type="submit">Add Tray App</button>
+                }} />
+            <FormField
+                control={form.control}
+                name="trayAppSrc"
+                render={({ field }) => {
+                    return <FormItem>
+                        <FormLabel>Path/URL</FormLabel>
+                        <FormControl>
+                            <Input {...field} autoComplete="off" />
+                        </FormControl>
+                        <FormDescription>The Path or URL that will be opened when the icon is clicked</FormDescription>
+                        <FormMessage />
+                    </FormItem>
 
-    </form>
+                }} />
+            {form.watch("trayAppIcon") && <Icon src={convertFileSrc(form.watch("trayAppIcon"))} altName="Selected icon" />}
+            <FormField
+                control={form.control}
+                name="trayAppIcon"
+                render={() => {
+                    return <FormItem className="flex flex-col">
+                        <FormLabel>App Icon</FormLabel>
+                        <FormControl className="w-min">
+                            <Button type="button" onClick={handleSelectImage}>
+                                Select Icon
+                            </Button>
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+
+                }} />
+
+            <Button type="submit">Add Tray App</Button>
+        </form>
+    </Form>
 }
