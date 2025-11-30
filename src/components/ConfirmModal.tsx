3d9311650d2722cb22501b9af360ecadd707ed9f
dubisdev <dubisdev@gmail.com>
2025-11-30 19:52:00 +0100

    chore: configure lint and formatter

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,23 +1,31 @@
-import { createCallable } from 'react-call'
-import { Dialog, DialogContent } from './ui/dialog'
-import { Button } from './ui/button'
-import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
+import { createCallable } from "react-call";
+import { Dialog, DialogContent } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 
-interface Props { message: string }
-type Response = boolean
+interface Props {
+  message: string;
+}
+type Response = boolean;
 
 export const Confirm = createCallable<Props, Response>(({ call, message }) => (
-  <Dialog defaultOpen onOpenChange={(value) => {
-    if (!value) call.end(false)
-  }}>
+  <Dialog
+    defaultOpen
+    onOpenChange={(value) => {
+      if (!value) call.end(false);
+    }}
+  >
     <DialogContent>
       <DialogTitle>Confirm</DialogTitle>
       <DialogDescription>{message}</DialogDescription>
       <div className="flex justify-end gap-4">
-        <Button variant="destructive" onClick={() => call.end(true)}>Yes</Button>
-        <Button variant="secondary" onClick={() => call.end(false)}>No</Button>
+        <Button variant="destructive" onClick={() => call.end(true)}>
+          Yes
+        </Button>
+        <Button variant="secondary" onClick={() => call.end(false)}>
+          No
+        </Button>
       </div>
     </DialogContent>
-
   </Dialog>
-))
+));
--- src/components/EditTrayAppActions.tsx
+++ src/components/EditTrayAppActions.tsx
@@ -7,110 +7,198 @@ import { TrayAppAction, TrayAppActions } from "../modules/TrayAppAction/TrayAppA
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./ui/form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-const EditTrayAppFormSchema = z.object({
-    trayAppName: z.string().nonempty(),
-    trayAppIcon: z.string().nonempty(),
-    trayAppSrc: z.string().nonempty(),
-})
+const EditTrayAppFormSchema = z.union([
+    z.object({
+        trayAppName: z.string().trim().nonempty(),
+        trayAppIcon: z.string().trim().nonempty(),
+        trayAppType: z.literal(TrayAppActions.CODE),
+        trayAppCode: z.string().trim().nonempty(),
+    }),
+    z.object({
+        trayAppName: z.string().trim().nonempty(),
+        trayAppIcon: z.string().trim().nonempty(),
+        trayAppType: z.literal(TrayAppActions.OPEN_PATH),
+        trayAppSrc: z.string().trim().nonempty(),
+    }),
+]);
 
-type IEditTrayAppForm = z.infer<typeof EditTrayAppFormSchema>
+type IEditTrayAppForm = z.infer<typeof EditTrayAppFormSchema>;
 
 interface CreateTrayAppFormProps {
-    trayApp: TrayApp
-    onUpdated: () => void
+    trayApp: TrayApp;
+    onUpdated: () => void;
 }
 
 export const UpdateTrayAppForm = ({ trayApp, onUpdated }: CreateTrayAppFormProps) => {
     const form = useForm<IEditTrayAppForm>({
         resolver: zodResolver(EditTrayAppFormSchema),
-        defaultValues: {
-            trayAppName: trayApp.name,
-            trayAppIcon: trayApp.iconSrc,
-            trayAppSrc: trayApp.action.configuration.path
-        }
-    })
+        defaultValues:
+            trayApp.action.type === TrayAppActions.OPEN_PATH
+                ? {
+                      trayAppName: trayApp.name,
+                      trayAppIcon: trayApp.iconSrc,
+                      trayAppSrc: trayApp.action.configuration.path,
+                      trayAppType: TrayAppActions.OPEN_PATH,
+                  }
+                : {
+                      trayAppName: trayApp.name,
+                      trayAppIcon: trayApp.iconSrc,
+                      trayAppCode: trayApp.action.configuration.code,
+                      trayAppType: TrayAppActions.CODE,
+                  },
+    });
 
-    const updateTrayApp = useTrayAppsStore(s => s.updateTrayApp)
+    const updateTrayApp = useTrayAppsStore((s) => s.updateTrayApp);
 
     const handleSelectImage = async () => {
-        const filePath = await open({ multiple: false, filters: [{ name: "*", extensions: ["png"] }] })
+        const filePath = await open({
+            multiple: false,
+            filters: [{ name: "*", extensions: ["png"] }],
+        });
 
-        if (!filePath) return
+        if (!filePath) return;
 
-        form.setValue("trayAppIcon", filePath)
-    }
+        form.setValue("trayAppIcon", filePath);
+    };
 
     const handleUpdateTrayApp: SubmitHandler<IEditTrayAppForm> = async (data) => {
-        const { trayAppName, trayAppIcon, trayAppSrc } = data
+        const { trayAppName, trayAppIcon, trayAppType } = data;
+        if (trayAppType === TrayAppActions.CODE) {
+            const { trayAppCode } = data;
+            const action: TrayAppAction = {
+                type: TrayAppActions.CODE,
+                configuration: {
+                    code: trayAppCode,
+                },
+            };
+
+            updateTrayApp({
+                ...trayApp,
+                name: trayAppName,
+                iconSrc: trayAppIcon,
+                action,
+            });
+
+            onUpdated();
+            return;
+        }
+
+        const { trayAppSrc } = data;
 
         const action: TrayAppAction = {
             type: TrayAppActions.OPEN_PATH,
             configuration: {
-                path: trayAppSrc
-            }
-        }
+                path: trayAppSrc,
+            },
+        };
 
         updateTrayApp({
             ...trayApp,
             name: trayAppName,
             iconSrc: trayAppIcon,
-            action
-        })
+            action,
+        });
 
-        onUpdated()
-    }
+        onUpdated();
+    };
 
-    return <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleUpdateTrayApp)} className="space-y-8">
-            <FormField
-                control={form.control}
-                name="trayAppName"
-                render={({ field }) => {
-                    return <FormItem>
-                        <FormLabel>App Name</FormLabel>
-                        <FormControl>
-                            <Input {...field} autoComplete="off" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(handleUpdateTrayApp, console.log)}
+                className="space-y-8"
+            >
+                <FormField
+                    control={form.control}
+                    name="trayAppName"
+                    render={({ field }) => {
+                        return (
+                            <FormItem>
+                                <FormLabel>App Name</FormLabel>
+                                <FormControl>
+                                    <Input {...field} autoComplete="off" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        );
+                    }}
+                />
+                {trayApp.action.type === TrayAppActions.OPEN_PATH && (
+                    <FormField
+                        control={form.control}
+                        name="trayAppSrc"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormLabel>Path/URL</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} autoComplete="off" />
+                                    </FormControl>
+                                    <FormDescription>
+                                        The Path or URL that will be opened when the icon is clicked
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            );
+                        }}
+                    />
+                )}
+                {trayApp.action.type === TrayAppActions.CODE && (
+                    <FormField
+                        control={form.control}
+                        name="trayAppCode"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormLabel>Code</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} autoComplete="off" />
+                                    </FormControl>
+                                    <FormDescription>
+                                        The code that will be executed when the icon is clicked
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            );
+                        }}
+                    />
+                )}
+                {form.watch("trayAppIcon") && (
+                    <Icon src={convertFileSrc(form.watch("trayAppIcon"))} altName="Selected icon" />
+                )}
 
-                }} />
-            <FormField
-                control={form.control}
-                name="trayAppSrc"
-                render={({ field }) => {
-                    return <FormItem>
-                        <FormLabel>Path/URL</FormLabel>
-                        <FormControl>
-                            <Input {...field} autoComplete="off" />
-                        </FormControl>
-                        <FormDescription>The Path or URL that will be opened when the icon is clicked</FormDescription>
-                        <FormMessage />
-                    </FormItem>
+                <FormField
+                    control={form.control}
+                    name="trayAppIcon"
+                    render={() => {
+                        return (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>App Icon</FormLabel>
+                                <FormControl className="w-min">
+                                    <Button type="button" onClick={handleSelectImage}>
+                                        Select Icon
+                                    </Button>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        );
+                    }}
+                />
 
-                }} />
-            {form.watch("trayAppIcon") && <Icon src={convertFileSrc(form.watch("trayAppIcon"))} altName="Selected icon" />}
-            <FormField
-                control={form.control}
-                name="trayAppIcon"
-                render={() => {
-                    return <FormItem className="flex flex-col">
-                        <FormLabel>App Icon</FormLabel>
-                        <FormControl className="w-min">
-                            <Button type="button" onClick={handleSelectImage}>
-                                Select Icon
-                            </Button>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-
-                }} />
-
-            <Button type="submit">Update Tray App</Button>
-        </form>
-    </Form>
-}
+                <Button type="submit">Update Tray App</Button>
+            </form>
+        </Form>
+    );
+};
